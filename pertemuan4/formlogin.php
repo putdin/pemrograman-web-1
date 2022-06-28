147b8be6df3aa9c5bba323d1342b7aa91cef178a
Moh Saifuddin <moh541fuddin@gmail.com>
2022-06-28 23:58:02 +0700

    form sudah ada

---
+++ pertemuan4/formlogin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>FORM LOGIN</title>
+</head>
+
+<body>
+  <form action="login.php" method="post"></form>
+  <table>
+    <tr>
+      <td>Username</td>
+      <td><input type="text" name="username"></td>
+    </tr>
+    <tr>
+      <td>Password</td>
+      <td><input type="text" name="password"></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td><input type="submit" name="login" value="Log In"></td>
+    </tr>
+  </table>
+</body>
+
+</html>
