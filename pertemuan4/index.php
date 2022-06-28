71e427fc002f77eca583210250ac5f7561d8809b
Moh Saifuddin <moh541fuddin@gmail.com>
2022-06-28 23:11:14 +0700

    pertemuan 4 kurang from login

---
+++ pertemuan4/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Hitung Luas Lingkaran</title>
+</head>
+
+<body>
+  <?php
+  /* ini adalah hasil sebelum perubahan
+  $nilai_1 = 10;
+  $nilai_2 = 3;
+  $nilai_3 = 2 * $nilai_1 + 8 * $nilai_2;
+
+  echo "Nilai = ", $nilai_3;
+  echo "<br>";
+
+  $jumlah = $nilai_1 + $nilai_2;
+  echo "Hasil dari $nilai_1 + $nilai_2 adalah : $jumlah";
+  echo "<br><br>";
+
+  echo "Nama : Moh Saifuddin <br>";
+  echo "Nim  : 112010130";
+  */
+
+  $jari = 14;
+  $luas = 22 / 7 * $jari * $jari;
+  echo "Luas lingkaran 25 adalah :  " . $luas;
+
+  ?>
+</body>
+
+</html>
